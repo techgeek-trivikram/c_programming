545bb7013f79644fa49290fb113d573126f2a9f0
techgeek-trivikram <trivikram.anand2021@vitstudent.ac.in>
2022-02-17 19:36:06 +0530

    First commit in github

---
+++ class17feb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.14
+
+
+int main()
+{
+int a=2,b;
+b = (a>0)?printf("A is greater than 0"):printf("A is less than 0");
+
+}
+
+if (a<b){
+
+}
+
+else if () {
+
+}
+
+else {
+
+}
+
---
+++ class17thfeb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.14
+
+int main()
+{
+
+
+int a=2,b;
+b = (a>0)?printf("A is greater than 0"):printf("")
+
+}
+
+
